ea4158a9658b121a3f1dd7b42be7aa041fb281cc
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-15 18:19:53 +1300

    application context setup

--- EFCoreTrainning/Domain/Customer.cs
+++ EFCoreTrainning/Domain/Customer.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EFCoreTrainning.Domain
 {   
+    [Table("Customers")]
     public class Customer
     {
+        [Key]
         public int Id {get; set;}
+        [Required]
         public string Name {get; set;}
+        [Column("Phone")]
         public string Phone {get; set;}
         public string PostCode {get; set;}
         public string State {get; set;}
